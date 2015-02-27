9422f8f8a04044ef94fb5490d32a6cb5cb7698f4
kedh <Eric@user227-25.wireless.utoronto.ca>
2015-02-27 06:20:10 -0500

    .

--- server/communityfund/urls.py
+++ server/communityfund/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from communityfund import settings
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
