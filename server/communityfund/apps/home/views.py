08c88fd0cc932422b598da907ba0f88ad3d91b5e
kedh <Eric@user227-25.wireless.utoronto.ca>
2015-02-27 05:46:48 -0500

    rending index.html

--- server/communityfund/apps/home/views.py
+++ server/communityfund/apps/home/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def index(request):
-	return HttpResponse("hello world")
+	#index_html = loader.get_template('index.html')
+	#return HttpResponse(index_html.render(...))
+
+	# simple html
+	return render_to_response('index.html')
--- server/communityfund/settings.py
+++ server/communityfund/settings.py
@@ -86,9 +86,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
-
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
