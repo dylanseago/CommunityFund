bcd7fa1b1b248dca7fca77d16344cb1e161624e2
kedh <Eric@Ericui-MacBook-Pro.local>
2015-03-01 14:53:54 -0500

    Migrated Search Model.py with manage.py sqlmigration

--- server/communityfund/apps/search/models.py
+++ server/communityfund/apps/search/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+	pID = models.IntegerField()
+	initiator = models.IntegerField()
+	name = models.TextField()
+	goal = models.IntegerField()
+	description = models.TextField()
+	timeToFund = models.DateField()
+
