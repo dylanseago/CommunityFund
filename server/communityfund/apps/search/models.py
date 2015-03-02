43cdd6dc20274c3714fa5929271bfe41cb05c751
kedh <Eric@Ericui-MacBook-Pro.local>
2015-03-02 02:20:34 -0500

    Organized search model database

--- server/communityfund/apps/search/models.py
+++ server/communityfund/apps/search/models.py
@@ -14,10 +14,32 @@ class User(models.Model):
 		except:
 			return None
 		return user
+
+	def update_User(uObj,test):
+		return True
+
+	@staticmethod
+	def get_User(uID=None, firstName=None, lastName=None, email=None, rating=None):
+		# Change like this later
+		# def get_User(*args, **kwargs):
+		user=[]
+		if (uID == None):
+			return None
+		user=User.objects.get(uID=uID)
+		return user
+
+	@staticmethod
+	def set_User(uID, firstName, lastName, email, rating):
+		user=User.create(uID=uID, firstName=firstName, lastName=lastName, email=email, rating=rating)
+		return user
+
+
 	def __unicode__(self):
 		return u'%s %s %s' %(self.uID, self.firstName, self.lastName)
+
 	def __str__(self):
 		return '%s %s %s' %(self.uID, self.firstName, self.lastName)
+
 
 class Project(models.Model):
 	pID = models.IntegerField(primary_key=True)
@@ -35,8 +57,41 @@ class Project(models.Model):
 		except:
 			return None
 		return project
+
+	@staticmethod
+	def get_Project(initiator=None, name=None):
+		if ((initiator == None) and (name == None)):
+			return None
+		project=[]
+		try:
+			if (name):
+				project=Project.objects.get(name=name)
+			else:
+				project=Project.objects.get(initiator=initiator)
+		except Project.DoesNotExist:
+			return None
+		return project
+
+	def update_Project(pObj, pID, initiator, name, goal, description, timeToFund):
+		pObj.pID=pID
+		pObj.initiator=initiator
+		pObj.name=name
+		pObj.goal=goal
+		pObj.description=description
+		pObj.timeToFund=timeToFund
+		try:
+			pObj.save()
+		except:
+			return None
+		return pObj
+	@staticmethod
+	def set_Project(pID, initiator, name, goal, description, timeToFund):
+		project=Project.create(pID=pID, initiator=initiator, name=name, goal=goal, description=description, timeToFund=timeToFund)
+		return project
+
 	def __unicode__(self):
 		return u'%s %s %s' %(self.pID, self.initiator, self.name)
+
 	def __str__(self):
 		return '%s %s %s' %(self.pID, self.initiator, self.name)
 
--- server/communityfund/apps/search/views.py
+++ server/communityfund/apps/search/views.py
@@ -8,39 +8,14 @@ def echo(request):
 	#project=set_Project(pID="0", initiator="0", name="test", goal="test", description="t", timeToFund="10:00:00")
 	#project=get_Project(project.objects.values('initiator'), project.objects.values('name'))
 	#html = "<html><body>echo_msg: %s.</body></html>" % request.GET.get('q', '')
-	user=User.objects.get(uID=0)
+	user=User.get_User(uID=0)
 	html=user.__str__()
+	if (User.update_User(user, 1)):
+		pass
+	if (user.update_User(1)):
+		pass	
 	return HttpResponse(html)
 
-def get_Project(initiator=None, name=None):
-	if (not initiator and not name):
-		return None
-	project=[]
-	try:
-		if (name):
-			project=Project.objects.get(name=name)
-		else:
-			project=Project.objects.get(initiator=initiator)
-	except Project.DoesNotExist:
-		return None
-	return project
-
-def set_Project(pID, initiator, name, goal, description, timeToFund):
-	project=Project.create(pID=pID, initiator=initiator, name=name, goal=goal, description=description, timeToFund=timeToFund)
-	return project
-
-def update_Project(pObj, pID, initiator, name, goal, description, timeToFund):
-	pObj.pID=pID
-	pObj.initiator=initiator
-	pObj.name=name
-	pObj.goal=goal
-	pObj.description=description
-	pObj.timeToFund=timeToFund
-	try:
-		pObj.save()
-	except:
-		return None
-	return pObj
 
 
 
