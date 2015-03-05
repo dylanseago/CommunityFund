cacab89c618ddd61753081b403266d31d315a7ba
dylanseago <dylan.seago@gmail.com>
2015-03-05 02:01:58 -0500

    Fixed format of urlconfs

--- server/communityfund/apps/home/urls.py
+++ server/communityfund/apps/home/urls.py
@@ -4,11 +4,11 @@ from communityfund.apps.home import views
 
 
 urlpatterns = patterns('',
-                       url(r'^/?$', views.index, name='index'),
-                       url(r'^about/?$', views.index, name='about'),
-                       url(r'^projects/?$', views.projects, name='projects'),
-                       url(r'^communities/?$', views.communities, name='communities'),
-                       url(r'^profile/?$', views.profile, name='profile'),
+   url(r'^/?$', views.index, name='index'),
+   url(r'^about/?$', views.index, name='about'),
+   url(r'^projects/?$', views.projects, name='projects'),
+   url(r'^communities/?$', views.communities, name='communities'),
+   url(r'^profile/?$', views.profile, name='profile'),
 )
 
 
--- server/communityfund/urls.py
+++ server/communityfund/urls.py
@@ -9,12 +9,11 @@ from communityfund import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^', include('home.urls')),
+    url(r'^', include('home.urls')),
     url(r'^admin/', include(admin.site.urls)),
-                       url(r'^search/?$', 'search.views.project_result', name='search'),
-                       url(r'^accounts/login/?$', 'django.contrib.auth.views.login', {'template_name': 'login.html'},
-                           name='login'),
-                       url(r'^accounts/register/?$', 'home.views.index', name='register')
+    url(r'^search/?$', 'search.views.project_result', name='search'),
+    url(r'^accounts/login/?$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^accounts/register/?$', 'home.views.index', name='register')
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 urlpatterns += staticfiles_urlpatterns()
 
